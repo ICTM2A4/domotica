3e09f4d85ea62ad239bc72094f5b47c556d3d84f
unclefrans <franstuinstra@hotmail.com>
2020-04-23 11:31:16 +0200

    Main.java erbij. en nu hopen dat hij het doet?

---
+++ src/main/java/nl/ictm4a/domotica/Main.java
@@ -0,0 +1,25 @@
+package nl.ictm4a.domotica;
+
+import com.mysql.cj.protocol.Resultset;
+
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+//        String url = "jdbc:mysql://localhost:3306?useLegacyDatetimeCode=false&serverTimezone=GMT";
+//        String user = "root";
+//        String password = "root";
+//        try {
+//            Connection myConn = DriverManager.getConnection(url, user, password);
+//
+//        } catch (SQLException e){
+//            e.printStackTrace();
+//        }
+    }
+
+}
