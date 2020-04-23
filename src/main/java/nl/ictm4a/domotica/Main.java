7dfc98297983190da3535f27842548bc30f237b1
JosDUisterhout <jos.duisterhout@outlook.com>
2020-04-23 13:49:11 +0200

    Frame muziekspeler en dialoog aanmaken playlist

--- src/main/java/nl/ictm4a/domotica/Main.java
+++ src/main/java/nl/ictm4a/domotica/Main.java
@@ -1,9 +1,15 @@
 package nl.ictm4a.domotica;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+
+
+        MusicPlayerFrame musicPlayerFrame = new MusicPlayerFrame();
+
     }
 
 }
